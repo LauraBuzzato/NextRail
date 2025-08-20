ee24f6c3b05b4906df130e62030a5a7d53c35da5
PotatoLur <guilherme.eguchi@sptech.school>
2025-08-19 21:26:36 -0300

    Finalizando a seção de sistema de monitoramento da tela inicial

--- public/js/script.js
+++ public/js/script.js
@@ -13,3 +13,30 @@ menu.addEventListener("mouseleave", () => {
   menu.classList.remove("active");
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    const sections = document.querySelectorAll('main');
+    const menuLinks = document.querySelectorAll('.menu a.link');
+    
+    function highlightMenu() {
+      let current = '';
+      
+      sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        
+        // Verifica se o topo da seção atingiu o topo da viewport
+        if (pageYOffset >= (sectionTop - 1)) {
+          current = section.getAttribute('id');
+        }
+      });
+      
+      menuLinks.forEach(link => {
+        link.parentElement.classList.remove('active');
+        if (link.getAttribute('href').substring(1) === current) {
+          link.parentElement.classList.add('active');
+        }
+      });
+    }
+    
+    window.addEventListener('scroll', highlightMenu);
+    highlightMenu();
+  });
