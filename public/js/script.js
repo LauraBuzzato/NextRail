9abb67cafdd36425dc5717213471be95e54334d8
ThaisKheyla <kheyla.paucara@sptech.school>
2025-08-26 12:09:48 -0300

    Adicionando Menu vertical

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,7 @@
-/*Ativar o menu muhehehhehe */
+/*Ativar o menu */
 
 const menu = document.querySelector(".menu");
 const linkItems = document.querySelectorAll(".link-item");
-// const darkMode = document.querySelector(".modo-escuro");
 
 menu.addEventListener("mouseenter", () => {
   menu.classList.add("active");
@@ -12,31 +11,3 @@ menu.addEventListener("mouseenter", () => {
 menu.addEventListener("mouseleave", () => {
   menu.classList.remove("active");
 });
-
-document.addEventListener('DOMContentLoaded', function() {
-    const sections = document.querySelectorAll('main');
-    const menuLinks = document.querySelectorAll('.menu a.link');
-    
-    function highlightMenu() {
-      let current = '';
-      
-      sections.forEach(section => {
-        const sectionTop = section.offsetTop;
-        
-        // Verifica se o topo da seção atingiu o topo da viewport
-        if (pageYOffset >= (sectionTop - 1)) {
-          current = section.getAttribute('id');
-        }
-      });
-      
-      menuLinks.forEach(link => {
-        link.parentElement.classList.remove('active');
-        if (link.getAttribute('href').substring(1) === current) {
-          link.parentElement.classList.add('active');
-        }
-      });
-    }
-    
-    window.addEventListener('scroll', highlightMenu);
-    highlightMenu();
-  });
