e5c523f071c8215351cc6bc2964b0d9e81b3936d
EnzoServilha <enzo.servilha@sptech.school>
2025-10-09 19:02:45 -0300

    implementação de outros graficos estáticos e botão de relatorio

---
+++ public/teste/adap_dash_cargo.js
@@ -0,0 +1,60 @@
+function analisaCargo() {
+    var cargo = sessionStorage.CARGO_USUARIO;
+    // o nome da variavel está atribuidas nas mudanças do corpo das páginas não em relação ao menu
+    // apesar de haver mudanças no menu não considero como alterações no corpo =)
+    var mudanca;
+
+    if (cargo == "Analista de infraestrutura") {
+        mudanca = false;
+    }else if (cargo == "Suporte técnico") {
+        mudanca = true;
+    }
+
+    atualizar(mudanca)
+}
+
+function atualizar(a) {
+    // if a = true então a página que logou é a do Suporte técnico
+    if (a == true) {
+        // mudando o menu de acordo com o cargo para disponibilidade
+        var li_relatorio = document.getElementById("relatorios");
+        li_relatorio.style.display = "none";
+
+        var li_configuracoes = document.getElementById("configuracoes")
+        li_configuracoes.style.display = "flex";
+
+        var li_cadastrar_server = document.getElementById("cadastroServer")
+        li_cadastrar_server.style.display = "flex";
+
+        // mudança no corpo
+        // lembrando de sempre colocar as classes dentro dos atributos para herdar o css
+        var main_conteudo_Principal = document.getElementById("conteudoPrincipal")
+        main_conteudo_Principal.innerHTML = `
+        <h1 class="titulo-destaque">Bem vindo(a) ${sessionStorage.NOME_USUARIO}</h1>
+        <div class="graficos-container">
+        <div class="grafico-box">
+          <h3>Uso de CPU</h3>
+          <canvas id="graficoRelatorioCPU"></canvas>
+        </div>
+        <div class="grafico-box">
+          <h3>Uso de RAM</h3>
+          <canvas id="graficoRelatorioRAM"></canvas>
+        </div>
+        <div class="grafico-box">
+          <h3>Uso de Disco</h3>
+          <canvas id="graficoRelatorioDisco"></canvas>
+        </div>
+      </div>
+        `
+        inicializarGraficos();
+        
+    }else {
+        
+        var li_configuracoes = document.getElementById("configuracoes")
+        li_configuracoes.style.display = "none";
+
+        var li_cadastrar_server = document.getElementById("cadastroServer")
+        li_cadastrar_server.style.display = "none";
+
+    }
+}
---
+++ public/teste/script.js
@@ -0,0 +1,23 @@
+
+const menu = document.querySelector(".menu");
+const linkItems = document.querySelectorAll(".link-item");
+
+menu.addEventListener("mouseenter", () => {
+  menu.classList.add("active");
+
+});
+
+menu.addEventListener("mouseleave", () => {
+  menu.classList.remove("active");
+});
+
+
+const hamburger = document.getElementById('hamburger');
+const menuH = document.querySelector('.menu');
+const main = document.querySelector('main');
+
+hamburger.addEventListener('click', () => {
+  menuH.classList.toggle('active');
+  hamburger.classList.toggle('active');
+  main.classList.toggle('shifted');
+});
