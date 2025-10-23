40407e202d526531e522525b41eb3e2cec6e8be2
jpPinheiro03 <joao.pinheiro@sptech.school>
2025-10-23 09:46:09 -0300

    cadastro do servidor junto com os componentes atualizado

--- src/models/servidorModel.js
+++ src/models/servidorModel.js
@@ -1,31 +1,31 @@
 var database = require("../database/config");
 
 function listarEmpresas() {
-  var instrucaoSql = `
+    var instrucaoSql = `
         SELECT id, razao_social FROM empresa;
     `;
-  console.log("Executando SQL: \n" + instrucaoSql);
-  return database.executar(instrucaoSql);
+    console.log("Executando SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
 }
 
 function listarTipos() {
-  var instrucaoSql = `
+    var instrucaoSql = `
         SELECT id, nome FROM tipo;
     `;
-  console.log("Executando SQL: \n" + instrucaoSql);
-  return database.executar(instrucaoSql);
+    console.log("Executando SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
 }
 
 function listarSO() {
-  var instrucaoSql = `
+    var instrucaoSql = `
         SELECT id, descricao FROM sistema_operacional;
     `;
-  console.log("Executando SQL: \n" + instrucaoSql);
-  return database.executar(instrucaoSql);
+    console.log("Executando SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
 }
 
 function selecionarServidores(fkEmpresa) {
-  var instrucaoSql = `
+    var instrucaoSql = `
         SELECT sv.nome AS servidor,tipo.nome AS tipo,so.descricao AS so,es.sigla AS estado,en.logradouro,en.numero,en.complemento 
         FROM servidor sv 
         INNER JOIN tipo ON tipo.id=sv.fk_tipo 
@@ -35,12 +35,12 @@ function selecionarServidores(fkEmpresa) {
         WHERE fk_empresa=${fkEmpresa}
         ORDER BY servidor;
     `;
-  console.log("Executando SQL: \n" + instrucaoSql);
-  return database.executar(instrucaoSql);
+    console.log("Executando SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
 }
 
 function listarServidores() {
-  var instrucaoSql = `
+    var instrucaoSql = `
         SELECT 
             s.id,
             s.nome,
@@ -52,38 +52,38 @@ function listarServidores() {
         INNER JOIN sistema_operacional so ON s.fk_so = so.id
         ORDER BY s.nome;
     `;
-  console.log("Executando SQL: \n" + instrucaoSql);
-  return database.executar(instrucaoSql);
+    console.log("Executando SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
 }
 
 function cadastrarServidor(nome, fk_tipo, fk_so, fk_empresa, logradouro, cep, numero, complemento, fk_estado) {
-    
+
     const complementoValue = complemento || null;
-    
+
     var instrucaoEndereco = `
         INSERT INTO endereco (logradouro, cep, numero, complemento, fk_estado) 
         VALUES ('${logradouro}', '${cep}', '${numero}', ${complementoValue ? `'${complementoValue}'` : 'NULL'}, ${fk_estado});
     `;
-    
+
     console.log("Executando SQL Endereço: \n" + instrucaoEndereco);
-    
+
     return database.executar(instrucaoEndereco)
         .then(resultadoEndereco => {
             const fk_endereco = resultadoEndereco.insertId;
             console.log("ID do endereço criado:", fk_endereco);
-            
+
             var instrucaoServidor = `
                 INSERT INTO servidor (nome, fk_tipo, fk_so, fk_endereco, fk_empresa) 
                 VALUES ('${nome}', ${fk_tipo}, ${fk_so}, ${fk_endereco}, ${fk_empresa});
             `;
-            
+
             console.log("Executando SQL Servidor: \n" + instrucaoServidor);
             return database.executar(instrucaoServidor);
         })
         .then(resultadoServidor => {
             const servidorId = resultadoServidor.insertId;
             console.log("ID do servidor criado:", servidorId);
-            
+
             return criarComponentesServidor(servidorId)
                 .then(() => resultadoServidor);
         })
@@ -92,46 +92,64 @@ function cadastrarServidor(nome, fk_tipo, fk_so, fk_empresa, logradouro, cep, nu
             throw erro;
         });
 }
-
 function criarComponentesServidor(servidorId) {
-    const componentes = ['CPU', 'Memória RAM', 'Disco Rígido'];
-    const gravidades = [1, 2, 3]; 
-    
+    // Mapeamento dos componentes para os IDs da tabela tipo_componente
+    // 1: 'Cpu', 2: 'Ram', 3: 'Disco'
+    const componentes = [
+        { nome: 'CPU', id: 1, metricaNome: 'Uso de CPU' },
+        { nome: 'Memória RAM', id: 2, metricaNome: 'Uso de Memória' },
+        { nome: 'Disco Rígido', id: 3, metricaNome: 'Espaço em Disco' }
+    ];
+
+    // IDs de gravidade, conforme a tabela gravidade
+    // 1: 'Baixo', 2: 'Médio', 3: 'Alto'
+    const gravidades = [1, 2, 3];
+
     let promises = [];
-    
+
     componentes.forEach(componente => {
+        // 1. INSERIR NA TABELA componente_servidor
+        // A inserção é feita uma vez para cada tipo de componente (CPU, RAM, Disco)
         var instrucaoComponente = `
-            INSERT INTO componente (nome, fk_servidor) 
-            VALUES ('${componente}', ${servidorId});
+            INSERT INTO componente_servidor (fk_servidor, fk_tipo_componente) 
+            VALUES (${servidorId}, ${componente.id});
         `;
-        
-        promises.push(
-            database.executar(instrucaoComponente)
-                .then(resultadoComponente => {
-                    const componenteId = resultadoComponente.insertId;
-                    console.log(`Componente ${componente} criado com ID:`, componenteId);
-                    
-                    let metricasPromises = [];
-                    gravidades.forEach(gravidade => {
-                        var nomeMetrica = componente === 'CPU' ? 'Uso de CPU' :
-                                         componente === 'Memória RAM' ? 'Uso de Memória' :
-                                         'Espaço em Disco';
-                        
-                        var instrucaoMetrica = `
-                            INSERT INTO metrica (fk_gravidade, nome, valor, fk_componente) 
-                            VALUES (${gravidade}, '${nomeMetrica}', 0, ${componenteId});
-                        `;
-                        
-                        metricasPromises.push(database.executar(instrucaoMetrica));
-                    });
-                    
-                    return Promise.all(metricasPromises);
-                })
-        );
+
+        // A Promise agora faz a inserção do componente E, em seguida, as métricas
+        const componentePromise = database.executar(instrucaoComponente)
+            .then(() => {
+                console.log(`Componente ${componente.nome} (ID ${componente.id}) criado para o Servidor ${servidorId}.`);
+
+                let metricasPromises = [];
+
+                // 2. INSERIR NA TABELA metrica
+                gravidades.forEach(gravidadeId => {
+                    var instrucaoMetrica = `
+                        INSERT INTO metrica (
+                            fk_gravidade, 
+                            nome, 
+                            valor, 
+                            fk_componenteServidor_servidor, 
+                            fk_componenteServidor_tipoComponente
+                        ) 
+                        VALUES (${gravidadeId}, '${componente.metricaNome}', 0, ${servidorId}, ${componente.id}
+                        );
+                    `;
+                    metricasPromises.push(database.executar(instrucaoMetrica));
+                });
+                return Promise.all(metricasPromises);
+            })
+            .catch(erro => {
+                console.error(`Erro ao criar componente ou métricas para ${componente.nome} no Servidor ${servidorId}:`, erro);
+                throw erro;
+            });
+
+        promises.push(componentePromise);
     });
-    
+
     return Promise.all(promises);
 }
+
 
 function atualizarConfiguracaoAlerta(servidorId, configuracoes) {
     return new Promise(async (resolve, reject) => {
@@ -163,19 +181,19 @@ function atualizarConfiguracaoAlerta(servidorId, configuracoes) {
                 INNER JOIN componente_servidor cs ON tc.id = cs.fk_tipo_componente
                 WHERE fk_servidor = ${servidorId};
             `;
-            
+
             const componentes = await database.executar(instrucaoComponentes);
-            
+
             if (componentes.length === 0) {
                 throw new Error('Nenhum componente encontrado para este servidor');
             }
-            
+
             let queries = [];
-            
+
             componentes.forEach(componente => {
                 let configComponente;
                 let nomeMetrica;
-                
+
                 if (componente.nome === 'Cpu') {
                     configComponente = configuracoes.cpu;
                     nomeMetrica = 'Uso de CPU';
@@ -188,7 +206,7 @@ function atualizarConfiguracaoAlerta(servidorId, configuracoes) {
                 } else {
                     return;
                 }
-                
+
                 if (configComponente && configComponente.baixo) {
                     queries.push(`
                         UPDATE metrica 
@@ -198,7 +216,7 @@ function atualizarConfiguracaoAlerta(servidorId, configuracoes) {
                         AND nome = '${nomeMetrica}'
                     `);
                 }
-                
+
                 if (configComponente && configComponente.medio) {
                     queries.push(`
                         UPDATE metrica 
@@ -208,7 +226,7 @@ function atualizarConfiguracaoAlerta(servidorId, configuracoes) {
                         AND nome = '${nomeMetrica}'
                     `);
                 }
-                
+
                 if (configComponente && configComponente.alto) {
                     queries.push(`
                         UPDATE metrica 
@@ -219,15 +237,15 @@ function atualizarConfiguracaoAlerta(servidorId, configuracoes) {
                     `);
                 }
             });
-            
+
             let results = [];
             for (let query of queries) {
                 const result = await database.executar(query);
                 results.push(result);
             }
-            
+
             resolve(results);
-            
+
         } catch (error) {
             reject(error);
         }
@@ -248,19 +266,19 @@ function buscarConfiguracoesServidor(servidorId) {
         WHERE cs.fk_servidor = ${servidorId}
         ORDER BY tc.nome_tipo_componente, g.id;
     `;
-    
+
     console.log("Buscando configurações do servidor: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
 module.exports = {
-  listarEmpresas,
-  listarTipos,
-  listarSO,
-  selecionarServidores,
-  listarServidores,
-  cadastrarServidor,
-  criarComponentesServidor,
-  atualizarConfiguracaoAlerta,
-  buscarConfiguracoesServidor
+    listarEmpresas,
+    listarTipos,
+    listarSO,
+    selecionarServidores,
+    listarServidores,
+    cadastrarServidor,
+    criarComponentesServidor,
+    atualizarConfiguracaoAlerta,
+    buscarConfiguracoesServidor
 };
