816b1a96643ea8c4bb1adb9aec35f0d813159847
Pedro-Luiz-Giraldi <pedro.giraldi@sptech.school>
2025-11-18 22:46:00 -0300

    alterações na página

--- public/js/dashAdmin.js
+++ public/js/dashAdmin.js
@@ -1,7 +1,7 @@
 let data = {
     Alto: {
         labels: ['21/10', '22/10', '23/10', '24/10', '25/10', '26/10', '27/10', '28/10', '29/10', '30/10'],
-        dados: [0.8, 0.7, 0.9, 1.2, 0.8, 0.9, 0.7, 0.9, 0.8, 0.9],
+        dados: [0.8, 0.7, 0.9, 1.2, 0.8, 0.8, 0.7, 0.8, 0.8, 0.9],
         cores: ['rgba(255,0,0)', 'rgba(255,0,0,0.4)'],
         sla: 1.0
     },
@@ -10,156 +10,82 @@ let data = {
         dados: [1.3, 1.5, 1.4, 1.7, 2.1, 1.9, 1.7, 1.5, 1.8, 1.9],
         cores: ['rgba(255,150,0)', 'rgba(255,150,0,0.4)'],
         sla: 2.0
+    },
+    Baixo: {
+        labels: ['21/10', '22/10', '23/10', '24/10', '25/10', '26/10', '27/10', '28/10', '29/10', '30/10'],
+        dados: [2.3, 2.5, 2.4, 2.5, 2.1, 2.7, 2.7, 2.5, 2.8, 2.6],
+        cores: ['rgba(255,255,0)', 'rgba(255,255,0,0.4)'],
+        sla: 3.0
     }
 }
+
 let atual = "Alto"
 
 let graficoSla = null;
-
-function mudarAlerta() {
-    console.log(atual)
-    if (atual == "Alto") {
-        atual = "Médio"
-    } else if (atual == "Médio") {
-        atual = "Baixo"
-    } else if (atual == "Baixo") {
-        atual = "Alto"
-    }
-    destruirGraficos()
-    criarGraficoSla()
-}
-
-function destruirGraficos() {
-    graficoSla.destroy()
-}
+let grafioTicket = null;
 
 function dashAdmin() {
-  console.log("Carregando gráficos...")
+    console.log("Carregando gráficos...")
 
-  //if (typeof Chart === 'undefined') {
-  //  console.log("erro ao cerregar grafico")
-  //  setTimeout(dashAdmin(), 500)
-  //  return
- // }
+    if (typeof Chart === 'undefined') {
+        console.log("erro ao cerregar grafico")
+        setTimeout(dashAdmin(), 500)
+        return
+    }
 
-  criarGraficoSla()
 
-  try {
-    //const [alertas] = await Promise.all([
-    //    fetch('/servidores/listarAlertas', {
-    //        method: "POST",
-    //        headers: { "Content-Type": "application/json" },
-    //        body: JSON.stringify({ idempresa: sessionStorage.ID_EMPRESA })
-    //    }).then(res => res.json())
-    //]) 
-    //console.log(alertas)
+    criarGraficoSla()
+    criarGraficoTicket()
+    try {
+        //const [alertas] = await Promise.all([
+        //    fetch('/servidores/listarAlertas', {
+        //        method: "POST",
+        //        headers: { "Content-Type": "application/json" },
+        //        body: JSON.stringify({ idempresa: sessionStorage.ID_EMPRESA })
+        //    }).then(res => res.json())
+        //]) 
+        //console.log(alertas)
 
-    // grafico tempo resolucao x sla
-    
-
-    // grafico ticketsSup
-    const ticketsSup = document.getElementById("ticketsSup");
-    new Chart(ticketsSup,
-    {
-      type: 'bar',
-      data: {
-        labels: ['João', 'Pedro', 'Matheus', 'Garbiel'],
-        datasets: [
-          {
-            label: 'Tickets Atribuidos',
-            data: [4, 3, 2, 1],
-            borderColor: 'blue',
-            backgroundColor: 'rgba(0, 0,250,0.4)',
-            tension: 0.3,
-            fill: true,
-            pointRadius: 4,
-            borderWidth: 2
-          },
-          //{
-          //  label: 'Tickets Resolvidos',
-          //  data: [4, 3, 3 ,0],
-          //  borderColor: 'green',
-          //  backgroundColor: 'rgba(0, 250, 0,0.4)',
-          //  tension: 0.3,
-          //  fill: true,
-          //  pointRadius: 4,
-          //  borderWidth: 2
-         // }
-        ]
-      },
-      options: {
-        indexAxis: 'y',
-        color: 'white',
-          scales: {
-                x: {
-                    title: {
-                        display: true,
-                        text: 'Tickets',
-                        color: 'white',
-                        font: {
-                            size: 20
-                        }
-                    },
-                    grid: { color: 'rgba(255,255,255,0.1)' }
-                },
-                y: {
-                    beginAtZero: true,
-                    title: {
-                        display: true,
-                        text: 'Membro do Suporte',
-                        color: 'white',
-                        font: {
-                            size: 20
-                        }
-                    },
-                    grid: { color: 'rgba(255,255,255,0.1)' }
-                }
-          }
-      },
-      borderColor: 'rgb(75, 192, 192)',
-      tension: 0.1
-      })
-  } catch(err) {
-    console.log("Erro ao carregar gráficos")
-  }
+    } catch(err) {
+        console.log("Erro ao carregar gráficos")
+    }
 }
 
 
 function criarGraficoSla() {
     const tempoSla = document.getElementById("tempoSla");
 
-    if (tempoSla && typeof Chart != 'undefined') {
-        graficoSla = new Chart(tempoSla,
+    graficoSla = new Chart(tempoSla,
         {
-          type: 'line',
-          data: {
-            labels: data[atual].labels, //LABELS
-            datasets: [
-              {
-                label: atual,
-                data: data[atual].dados,
-                borderColor: data[atual].cores[0],
-                backgroundColor: data[atual].cores[1],
-                tension: 0.3,
-                fill: true,
-                pointRadius: 4,
-                borderWidth: 2
-              },
-              {
-                  label: 'limite SLA',
-                  data: Array(13).fill(data[atual].sla),
-                  borderColor: '#a78bfa',
-                  backgroundColor: 'rgba(167,139,250,0.2)',
-                  tension: 0.4,
-                  fill: false,
-                  pointRadius: 0,
-                  datalabels: { display: false }
-              }
-            ]
-          },
-          options: {
-            color: 'white',
-              scales: {
+            type: 'line',
+            data: {
+                labels: data[atual].labels, //LABELS
+                datasets: [
+                    {
+                        label: atual,
+                        data: data[atual].dados,
+                        borderColor: data[atual].cores[0],
+                        backgroundColor: data[atual].cores[1],
+                        tension: 0.3,
+                        fill: true,
+                        pointRadius: 4,
+                        borderWidth: 2
+                    },
+                    {
+                        label: 'limite SLA',
+                        data: Array(13).fill(data[atual].sla),
+                        borderColor: '#a78bfa',
+                        backgroundColor: 'rgba(167,139,250,0.2)',
+                        tension: 0.4,
+                        fill: false,
+                        pointRadius: 0,
+                        datalabels: { display: false }
+                    }
+                ]
+            },
+            options: {
+                color: 'white',
+                scales: {
                     x: {
                         title: {
                             display: true,
@@ -183,12 +109,75 @@ function criarGraficoSla() {
                         },
                         grid: { color: 'rgba(255,255,255,0.1)' }
                     }
-              }
-          },
-          borderColor: 'rgb(75, 192, 192)',
-          tension: 0.1
+                }
+            },
+            borderColor: 'rgb(75, 192, 192)',
+            tension: 0.1
         })
-    }
+}
+
+function criarGraficoTicket() {
+    const ticketsSup = document.getElementById("ticketsSup");
+    grafioTicket = new Chart(ticketsSup,
+        {
+            type: 'bar',
+            data: {
+                labels: ['João', 'Pedro', 'Matheus', 'Garbiel'],
+                datasets: [
+                    {
+                        label: 'Tickets Atribuidos',
+                        data: [4, 3, 2, 1],
+                        borderColor: 'blue',
+                        backgroundColor: 'rgba(0, 0,250,0.8)',
+                        tension: 0.3,
+                        fill: true,
+                        pointRadius: 4,
+                        borderWidth: 2
+                    },
+                    {
+                      label: 'Tickets Resolvidos',
+                      data: [4, 3, 1 ,2],
+                      borderColor: 'green',
+                      backgroundColor: 'rgba(0, 250, 0,0.8)',
+                      tension: 0.3,
+                      fill: true,
+                      pointRadius: 4,
+                     borderWidth: 2
+                    }
+                ]
+            },
+            options: {
+                indexAxis: 'y',
+                color: 'white',
+                scales: {
+                    x: {
+                        title: {
+                            display: true,
+                            text: 'Tickets',
+                            color: 'white',
+                            font: {
+                                size: 20
+                            }
+                        },
+                        grid: { color: 'rgba(255,255,255,0.1)' }
+                    },
+                    y: {
+                        beginAtZero: true,
+                        title: {
+                            display: true,
+                            text: 'Membro do Suporte',
+                            color: 'white',
+                            font: {
+                                size: 20
+                            }
+                        },
+                        grid: { color: 'rgba(255,255,255,0.1)' }
+                    }
+                }
+            },
+            borderColor: 'rgb(75, 192, 192)',
+            tension: 0.1
+        })
 }
 
 function mudarCor() {
@@ -205,7 +194,24 @@ function mudarCor() {
   }
 }
 
+function mudarAlerta() {
+    console.log(atual)
+    if (atual == "Alto") {
+        atual = "Médio"
+    } else if (atual == "Médio") {
+        atual = "Baixo"
+    } else if (atual == "Baixo") {
+        atual = "Alto"
+    }
+    destruirGraficos()
+    criarGraficoSla()
+    criarGraficoTicket()
+}
 
+function destruirGraficos() {
+    graficoSla.destroy()
+    grafioTicket.destroy()
+}
 //{
 //                label: 'Médio',
 //                data: [],
