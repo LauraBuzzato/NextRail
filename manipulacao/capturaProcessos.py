c57d06c3d58c60c1e57cf2b4583c6cd4c04ba968
jpPinheiro03 <joao.pinheiro@sptech.school>
2025-12-07 16:37:04 -0300

    mudando .env do python

--- manipulacao/capturaProcessos.py
+++ manipulacao/capturaProcessos.py
@@ -14,7 +14,7 @@ def get_daily_s3_key():
     return f"nomeProcessosUso_{today}.csv"
 
 
-load_dotenv(dotenv_path=".env.dev")
+load_dotenv(dotenv_path=".env")
 
 
 BUCKET_NAME = "nextrail-raw-log"
--- src/script/s3.js
+++ src/script/s3.js
@@ -2,7 +2,16 @@ const AWS = require('aws-sdk');
 const Papa = require('papaparse');
 
 AWS.config.update({ region: process.env.AWS_REGION });
-const s3 = new AWS.S3();
+
+const s3 = new S3Client({
+    region: process.env.AWS_DEFAULT_REGION,
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        sessionToken: process.env.AWS_SESSION_TOKEN
+    },
+    useAccelerateEndpoint: true,
+});
 
 async function lerArquivo(req, res) {
   try {
